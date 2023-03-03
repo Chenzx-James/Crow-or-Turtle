f182991acf9af3385882f526bd5fd8f48517b70d
Chenzx-James <chenzixuanjames@outlook.com>
2023-03-03 21:24:16 +0800

    Initial Commit

---
+++ Crow-or-Turtle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Crow-or-Turtle
+//
+//  Created by Chen Zixuan on 3.3.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
